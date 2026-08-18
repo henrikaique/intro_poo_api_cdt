3143f93a9d1947d4425d340464a18983105d7325
mariaeverissimo24-lab <henrikaique121@gmail.com>
2026-08-18 16:39:42 -0300

    update

---
+++ modulo08_meudesenho_henriqueaugusto.py
@@ -0,0 +1,34 @@
+'''
+Pokemon - Ash
+Naruto - Obito
+Ben 10 - Ben
+
+'''
+
+class BonecoToyStory:
+    
+    def __init__(self, nome, dono, frase_de_efeito):
+        self.nome = nome
+        self.dono = dono
+        self.frase_de_efeito  = frase_de_efeito
+        
+        
+        
+woody = BonecoToyStory(
+    nome = "Woody",
+    dono = "Andy",
+    frase_de_efeito = "Há uma cobra na minha bota!"
+)
+
+buzz = BonecoToyStory(
+    nome = "Buzz lightyear",
+    dono = "Andy",
+    frase_de_efeito = "Ao infinito e além"
+
+)
+
+rex = BonecoToyStory(
+    nome = "Rex",
+    dono = "Andy",
+    frase_de_efeito = "Eu não gosto de confrontos."
+)
